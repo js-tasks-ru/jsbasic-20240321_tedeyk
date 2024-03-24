5976e2de54e4150b9f0e24b224bc43683e71c120
Andrey Sidorov <tedeyk@mail.ru>
2024-03-24 18:40:50 +0300

    Подправил замечания

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,10 +1,12 @@
 function factorial(n) {
-     let numFac = 1;
-      for ( let i=1; i<=n; i++){
-      numFac=numFac*i 
-      }
-      return numFac;
-  
+  let numFac = 1;
+
+  for (let i=1; i<=n; i++) {
+      numFac = numFac * i; 
   }
-     factorial()
+
+  return numFac;
+
+}
+    
   
